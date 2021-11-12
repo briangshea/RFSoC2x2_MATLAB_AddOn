dcc9c770a9efef43598df89599cd661528d0774b
Brian G. Shea <bgshea@gmail.com>
2021-11-12 14:03:42 -0800

    Improved HDL Workflow parameters Added support for both ADCs on RFSoC2x2 Started to add NCO Ports for ADC - incomplete Reworked Vivado Block Diagram to maintain same heirarchy as original RFSoC2x2 Removed PMODs and some other items that are not useful to MATLAB

--- toolbox/shared/sdr/platform/derived/common/+sdr/+internal/+hdlwa/+vivado/+ip/+BGS/common/system_cleanup.tcl
+++ toolbox/shared/sdr/platform/derived/common/+sdr/+internal/+hdlwa/+vivado/+ip/+BGS/common/system_cleanup.tcl
@@ -12,7 +12,65 @@ if { [llength [current_bd_design -quiet]] == 0 } {
     open_bd_design $BDFILEPATH
 }
 
+#Disable the ADC NCO Ports on rfcd if they are not enabled
+if { $enable_adc_nco == "false" } {
+    set_property -dict [list CONFIG.ADC_NCO_RTS {false}] [get_bd_cells radio/rfdc]
+}
+
+#Disable the DAC NCO Ports on rfcd if they are not enabled
+#if { $enable_dac_nco == "false" } {
+#    set_property -dict [list CONFIG.DAC_NCO_RTS {false}] [get_bd_cells radio/rfdc]
+#}
+
 #source $mw_hdl_dir/projects/ad9361/common/mw_cleanup_io.tcl
+
+# Connected Unused AXIS Streams
+if { $adc0_enable == "false" } {
+    connect_bd_intf_net [get_bd_intf_pins radio/M00_AXIS_RE] -boundary_type upper [get_bd_intf_pins radioS00_AXIS_RE]
+    connect_bd_intf_net [get_bd_intf_pins radio/M00_AXIS_IM] -boundary_type upper [get_bd_intf_pins radioS00_AXIS_IM]
+set_property -dict [list CONFIG.C_S_AXIS_DATA_WIDTH {128}] [get_bd_cells radio/receiver/channel00/packet_generator]
+} else {
+    switch $adc0_axis_master_width {
+        8 {
+        set axis_m_width 8
+        } 16 {
+        set axis_m_width 16
+        } 32 {
+        set axis_m_width 32
+        } 64 {
+        set axis_m_width 64
+        } 128 {
+        set axis_m_width 128
+        } defalut {
+        set axis_m_width 128
+        }
+    }
+        
+    set_property -dict [list CONFIG.C_S_AXIS_DATA_WIDTH $axis_m_width] [get_bd_cells radio/receiver/channel00/packet_generator]
+}
+
+if { $adc2_enable == "false" } {
+    connect_bd_intf_net [get_bd_intf_pins radio/M20_AXIS_RE] -boundary_type upper [get_bd_intf_pins radio/S20_AXIS_RE]
+    connect_bd_intf_net [get_bd_intf_pins radio/M20_AXIS_IM] -boundary_type upper [get_bd_intf_pins radio/S20_AXIS_IM]
+    set_property -dict [list CONFIG.C_S_AXIS_DATA_WIDTH {128}] [get_bd_cells radio/receiver/channel20/packet_generator]
+} else {
+    switch $adc2_axis_master_width {
+        8 {
+        set axis_m_width 8
+        } 16 {
+        set axis_m_width 16
+        } 32 {
+        set axis_m_width 32
+        } 64 {
+        set axis_m_width 64
+        } 128 {
+        set axis_m_width 128
+        } defalut {
+        set axis_m_width 128
+        }
+    }
+    set_property -dict [list CONFIG.C_S_AXIS_DATA_WIDTH $axis_m_width] [get_bd_cells radio/receiver/channel20/packet_generator]
+}
 
 #source $mw_hdl_dir/projects/ad9361/common/mw_cleanup_bd_gpio.tcl
 
